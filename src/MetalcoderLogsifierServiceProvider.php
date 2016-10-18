b186b0fd35111e1a9bc8a9998d4e647d58747d05
yunior2016 <yunior@thecomicsfactory.com>
2016-10-18 15:55:09 -0400

    Updating service provider

--- src/MetalcoderLogsifierServiceProvider.php
+++ src/MetalcoderLogsifierServiceProvider.php
@@ -17,7 +17,7 @@ class MetalcoderLogsifierServiceProvider extends ServiceProvider
         //Publish migrations
        // $this->publishes([__DIR__ . '/migrations/2016_05_17_183336_create_pd_log_table.php' => base_path('database/migrations/2016_05_17_183336_create_pd_log_table.php')]);
 
-        $this->loadMigrationsFrom(__DIR__.'/src/migrations/');
+        $this->loadMigrationsFrom(__DIR__.'/migrations/');
         
     }
 
